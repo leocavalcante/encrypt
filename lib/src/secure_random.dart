660aab647026f8b1f1681d36bc94c699b6fa3f73
leocavalcante <lc@leocavalcante.com>
2019-08-31 13:00:00 -0300

    Move secure random generation to the lib

---
+++ lib/src/secure_random.dart
@@ -0,0 +1,21 @@
+part of encrypt;
+
+class SecureRandom {
+  static final Random _generator = Random.secure();
+  final Uint8List _bytes;
+
+  SecureRandom(int length)
+      : _bytes = Uint8List.fromList(
+            List.generate(length, (i) => _generator.nextInt(2 ^ 32)));
+
+  Uint8List get bytes => _bytes;
+
+  String get base16 =>
+      _bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join();
+
+  String get base64 => convert.base64.encode(_bytes);
+
+  String get utf8 => convert.utf8.decode(_bytes);
+
+  int get length => _bytes.length;
+}
