23eb8392184dee2223c7f76b93acc60b2877f636
farukprogrammer <faruqy@gmail.com>
2022-01-25 07:35:06 +0700

    upversion and update example avoid using fromLength

--- example/aes.dart
+++ example/aes.dart
@@ -3,8 +3,8 @@ import 'package:encrypt/encrypt.dart';
 void main() {
   final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
 
-  final key = Key.fromLength(32);
-  final iv = IV.fromLength(16);
+  final key = Key.fromSecureRandom(32);
+  final iv = IV.fromSecureRandom(16);
   final encrypter = Encrypter(AES(key));
 
   final encrypted = encrypter.encrypt(plainText, iv: iv);
